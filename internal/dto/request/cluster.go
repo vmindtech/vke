b9270484e5584e9f1e7733b2adf18484f929c58f
kaptanoglu <kaptanoglu@kaptanoglus-MacBook-Pro.local>
2023-11-13 12:33:37 +0300

    fix some  problem

--- internal/dto/request/cluster.go
+++ internal/dto/request/cluster.go
@@ -13,25 +13,33 @@ type CreateClusterRequest struct {
 }
 
 type CreateComputeRequest struct {
-	Server struct {
-		Name             string `json:"name"`
-		ImageRef         string `json:"imageRef"`
-		FlavorRef        string `json:"flavorRef"`
-		KeyName          string `json:"key_name"`
-		AvailabilityZone string `json:"availability_zone"`
-		SecurityGroups   []struct {
-			Name string `json:"name"`
-		} `json:"security_groups"`
-		BlockDeviceMappingV2 []struct {
-			BootIndex           int    `json:"boot_index"`
-			UUID                string `json:"uuid"`
-			SourceType          string `json:"source_type"`
-			DestinationType     string `json:"destination_type"`
-			DeleteOnTermination bool   `json:"delete_on_termination"`
-		} `json:"block_device_mapping_v2"`
-		Networks []struct {
-			UUID string `json:"uuid"`
-		} `json:"networks"`
-		UserData string `json:"user_data"`
-	} `json:"server"`
+	Server Server `json:"server"`
+}
+
+type Server struct {
+	Name                 string                 `json:"name"`
+	ImageRef             string                 `json:"imageRef"`
+	FlavorRef            string                 `json:"flavorRef"`
+	KeyName              string                 `json:"key_name"`
+	AvailabilityZone     string                 `json:"availability_zone"`
+	SecurityGroups       []SecurityGroups       `json:"security_groups"`
+	BlockDeviceMappingV2 []BlockDeviceMappingV2 `json:"block_device_mapping_v2"`
+	Networks             []Networks             `json:"networks"`
+	UserData             string                 `json:"user_data"`
+}
+
+type BlockDeviceMappingV2 struct {
+	BootIndex           int    `json:"boot_index"`
+	UUID                string `json:"uuid"`
+	SourceType          string `json:"source_type"`
+	DestinationType     string `json:"destination_type"`
+	DeleteOnTermination bool   `json:"delete_on_termination"`
+}
+
+type Networks struct {
+	Port string `json:"port"`
+}
+
+type SecurityGroups struct {
+	Name string `json:"name"`
 }
