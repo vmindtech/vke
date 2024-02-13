ff192794a5f61540311e24357d3e0665427c9c87
kubilaykaptanoglu <kubilay@kaptanoglu.me>
2024-02-13 13:50:09 +0300

    delete master node groups for return nodegroups

--- internal/repository/node_groups.go
+++ internal/repository/node_groups.go
@@ -37,7 +37,7 @@ func (n *NodeGroupsRepository) GetNodeGroupsByClusterUUID(ctx context.Context, u
 		Database().
 		WithContext(ctx).
 		Where(queryModel).
-		Where("node_groups_type == worker").
+		Where(model.NodeGroups{NodeGroupsType: "worker"}).
 		Find(&nodeGroups).
 		Error
 
