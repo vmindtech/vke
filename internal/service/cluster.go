03ba8e0732136c4e4bc898adb54a082871d4f6df
kubilaykaptanoglu <kubilay@kaptanoglu.me>
2024-02-22 10:37:29 +0300

    change first worker-group

--- internal/service/cluster.go
+++ internal/service/cluster.go
@@ -233,7 +233,7 @@ func (c *clusterService) CreateCluster(ctx context.Context, authToken string, re
 	workerNodeGroupModel := &model.NodeGroups{
 		ClusterUUID:         clusterUUID,
 		NodeGroupUUID:       workerServerGroupResp.ServerGroup.ID,
-		NodeGroupName:       "worker",
+		NodeGroupName:       "vke-worker-group",
 		NodeGroupMinSize:    req.WorkerNodeGroupMinSize,
 		NodeGroupMaxSize:    req.WorkerNodeGroupMaxSize,
 		NodeDiskSize:        req.WorkerDiskSizeGB,
@@ -806,7 +806,7 @@ func (c *clusterService) CreateCluster(ctx context.Context, authToken string, re
 			return resource.CreateClusterResponse{}, err
 		}
 		WorkerRequest.Server.Networks[0].Port = portResp.Port.ID
-		WorkerRequest.Server.Name = workerNodeGroupModel.NodeGroupName
+		WorkerRequest.Server.Name = fmt.Sprintf("%s-%s", workerNodeGroupModel.NodeGroupName, uuid.New().String())
 
 		_, err = c.computeService.CreateCompute(ctx, authToken, *WorkerRequest)
 		if err != nil {
