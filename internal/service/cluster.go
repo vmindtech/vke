63e9563962b534ba15ffad29d8dfc338a8047053
kubilaykaptanoglu <kubilay@kaptanoglu.me>
2024-02-22 10:16:29 +0300

    change first worker-group

--- internal/service/cluster.go
+++ internal/service/cluster.go
@@ -233,7 +233,7 @@ func (c *clusterService) CreateCluster(ctx context.Context, authToken string, re
 	workerNodeGroupModel := &model.NodeGroups{
 		ClusterUUID:         clusterUUID,
 		NodeGroupUUID:       workerServerGroupResp.ServerGroup.ID,
-		NodeGroupName:       "vke-worker-group",
+		NodeGroupName:       "worker",
 		NodeGroupMinSize:    req.WorkerNodeGroupMinSize,
 		NodeGroupMaxSize:    req.WorkerNodeGroupMaxSize,
 		NodeDiskSize:        req.WorkerDiskSizeGB,
