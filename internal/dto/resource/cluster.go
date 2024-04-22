8d4eab28e9b8722ac58fa4fda3ea30fccd50c115
kubilaykaptanoglu <kubilay@kaptanoglu.me>
2024-04-22 09:25:27 +0300

    incorrect logging was deleted and add clusterName field for response

--- internal/dto/resource/cluster.go
+++ internal/dto/resource/cluster.go
@@ -28,6 +28,7 @@ type GetClusterDetailsResponse struct {
 }
 
 type GetClusterResponse struct {
+	ClusterName                string `json:"clusterName"`
 	ClusterID                  string `json:"clusterId"`
 	ProjectID                  string `json:"projectId"`
 	KubernetesVersion          string `json:"kubernetesVersion"`
