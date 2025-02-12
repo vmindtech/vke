4f7c1c269bfbbcfdcdf76e916d823551b8958eb7
Mert Öngengil <mertongngl@gmail.com>
2025-02-12 17:41:38 +0300

    Add certificate expiration date to cluster responses (#53)

--- internal/dto/resource/cluster.go
+++ internal/dto/resource/cluster.go
@@ -14,17 +14,18 @@ type CreateClusterResponse struct {
 }
 
 type GetClusterDetailsResponse struct {
-	ClusterUUID               string      `json:"cluster_uuid"`
-	ClusterName               string      `json:"cluster_name"`
-	ClusterVersion            string      `json:"cluster_version"`
-	ClusterStatus             string      `json:"cluster_status"`
-	ClusterProjectUUID        string      `json:"cluster_project_uuid"`
-	ClusterLoadbalancerUUID   string      `json:"cluster_loadbalancer_uuid"`
-	ClusterMasterServerGroup  NodeGroup   `json:"cluster_master_server_group_uuid"`
-	ClusterWorkerServerGroups []NodeGroup `json:"cluster_worker_server_groups_uuid"`
-	ClusterSubnets            []string    `json:"cluster_subnets"`
-	ClusterEndpoint           string      `json:"cluster_endpoint"`
-	ClusterAPIAccess          string      `json:"cluster_api_access"`
+	ClusterUUID                  string      `json:"cluster_uuid"`
+	ClusterName                  string      `json:"cluster_name"`
+	ClusterVersion               string      `json:"cluster_version"`
+	ClusterStatus                string      `json:"cluster_status"`
+	ClusterProjectUUID           string      `json:"cluster_project_uuid"`
+	ClusterLoadbalancerUUID      string      `json:"cluster_loadbalancer_uuid"`
+	ClusterMasterServerGroup     NodeGroup   `json:"cluster_master_server_group_uuid"`
+	ClusterWorkerServerGroups    []NodeGroup `json:"cluster_worker_server_groups_uuid"`
+	ClusterSubnets               []string    `json:"cluster_subnets"`
+	ClusterEndpoint              string      `json:"cluster_endpoint"`
+	ClusterAPIAccess             string      `json:"cluster_api_access"`
+	ClusterCertificateExpireDate time.Time   `json:"cluster_certificate_expire_date"`
 }
 
 type GetClusterResponse struct {
