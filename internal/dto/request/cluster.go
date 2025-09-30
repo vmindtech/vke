312376a4446c2d6b8330c5a676e204691d47ac09
Mert Öngengil <mertongngl@gmail.com>
2025-09-30 15:59:18 +0300

    Update validation rules for cluster request DTOs

--- internal/dto/request/cluster.go
+++ internal/dto/request/cluster.go
@@ -3,33 +3,33 @@ package request
 import "time"
 
 type CreateClusterRequest struct {
-	ClusterName              string   `json:"clusterName"`
-	ProjectID                string   `json:"projectId"`
-	KubernetesVersion        string   `json:"kubernetesVersion"`
-	NodeKeyPairName          string   `json:"nodeKeyPairName"`
-	ClusterAPIAccess         string   `json:"clusterApiAccess"`
-	SubnetIDs                []string `json:"subnetIds"`
-	WorkerNodeGroupMinSize   int      `json:"workerNodeGroupMinSize"`
-	WorkerNodeGroupMaxSize   int      `json:"workerNodeGroupMaxSize"`
-	WorkerInstanceFlavorUUID string   `json:"workerInstanceFlavorUUID"`
-	MasterInstanceFlavorUUID string   `json:"masterInstanceFlavorUUID"`
-	WorkerDiskSizeGB         int      `json:"workerDiskSizeGB"`
-	AllowedCIDRS             []string `json:"allowedCIDRs"`
+	ClusterName              string   `json:"clusterName" validate:"required,max=50"`
+	ProjectID                string   `json:"projectId" validate:"required"`
+	KubernetesVersion        string   `json:"kubernetesVersion" validate:"required,max=30"`
+	NodeKeyPairName          string   `json:"nodeKeyPairName" validate:"required,max=140"`
+	ClusterAPIAccess         string   `json:"clusterApiAccess" validate:"required,max=255"`
+	SubnetIDs                []string `json:"subnetIds" validate:"required"`
+	WorkerNodeGroupMinSize   int      `json:"workerNodeGroupMinSize" validate:"required,min=1"`
+	WorkerNodeGroupMaxSize   int      `json:"workerNodeGroupMaxSize" validate:"required,min=1"`
+	WorkerInstanceFlavorUUID string   `json:"workerInstanceFlavorUUID" validate:"required"`
+	MasterInstanceFlavorUUID string   `json:"masterInstanceFlavorUUID" validate:"required"`
+	WorkerDiskSizeGB         int      `json:"workerDiskSizeGB" validate:"required,min=20"`
+	AllowedCIDRS             []string `json:"allowedCIDRs" validate:"required"`
 }
 
 type CreateKubeconfigRequest struct {
-	ClusterID  string `json:"clusterId"`
+	ClusterID  string `json:"clusterId" validate:"required"`
 	KubeConfig string `json:"kubeconfig"`
 }
 
 type UpdateKubeconfigRequest struct {
-	KubeConfig string `json:"kubeconfig"`
+	KubeConfig string `json:"kubeconfig" validate:"required"`
 }
 
 type UpdateClusterRequest struct {
-	ClusterName                  string    `json:"cluster_name"`
-	ClusterVersion               string    `json:"cluster_version"`
-	ClusterStatus                string    `json:"cluster_status"`
-	ClusterAPIAccess             string    `json:"cluster_api_access"`
-	ClusterCertificateExpireDate time.Time `json:"cluster_certificate_expire_date"`
+	ClusterName                  string    `json:"cluster_name" validate:"required,max=50"`
+	ClusterVersion               string    `json:"cluster_version" validate:"omitempty,max=30"`
+	ClusterStatus                string    `json:"cluster_status" validate:"required,max=10"`
+	ClusterAPIAccess             string    `json:"cluster_api_access" validate:"required,max=255"`
+	ClusterCertificateExpireDate time.Time `json:"cluster_certificate_expire_date" validate:"required"`
 }
